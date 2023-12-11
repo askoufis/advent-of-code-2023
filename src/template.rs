25ec6986e91f1fab68780522e86da53cfb05a668
Adam Skoufis <adam.skoufis@gmail.com>
2023-12-11 20:48:16 +1100

    Fix up template

--- src/template.rs
+++ src/template.rs
@@ -4,12 +4,12 @@ pub fn input_generator(input: &str) -> Vec<usize> {
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &[u32]) -> usize {
+pub fn solve_part1(input: &[usize]) -> usize {
     1
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &[u32]) -> usize {
+pub fn solve_part2(input: &[usize]) -> usize {
     1
 }
 
